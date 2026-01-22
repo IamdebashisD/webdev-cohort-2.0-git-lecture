6a0740e60bcb09b2606422342551f97f151e7c0f
IamdebashisD <adebashisdas626@gmail.com>
2026-01-22 22:45:28 +0530

    I have just merge changes from feat a branch

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function add(a, b){
     return a + b
 }
+function sub(a, b){
+    if(!(a>b)) throw new Error('num shoukld be greater than b')
+    return a - b
+}
 
 add(2, 4)
 add(10, 23)
 add(20, 5)
+sub(3-1)
